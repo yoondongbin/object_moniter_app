e18d6d7d47df3ec2514f3f0a0a2173284d5bc2b2
yoondongbin <wkrlvkrja@gmail.com>
2025-07-31 11:22:24 +0900

    api분리 완료

--- mobile/src/screens/StatsScreen.tsx
+++ mobile/src/screens/StatsScreen.tsx
@@ -1,20 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import styles from '../styles/StatsScreen.styles';
 import WeekSelector from '../components/WeekSelector';
 import Chart from '../components/Chart';
-import { dummyStats } from '../data/dummyStats';
-import { DailyStats } from '../data/dummyStats';
-import { getStatsByWeekOffset } from '../utils/statsUtils';
+import { getStatsByWeek, type DailyStats } from '../services/api/statsApi';
 
 export default function StatsScreen() {
-    const [selectedWeekOffset, setSelectedWeekOffset] = useState(0); // 0: 이번주, 1: 지난주, 2: 그저께, 3: 그저저번주
-    const fileteredData: DailyStats[] = getStatsByWeekOffset(dummyStats, selectedWeekOffset); 
+  const [selectedWeekOffset, setSelectedWeekOffset] = useState(0);
+  const [data, setData] = useState<DailyStats[]>([]);
+
+  useEffect(() => {
+    getStatsByWeek(selectedWeekOffset).then(setData);
+  }, [selectedWeekOffset]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>주간 탐지 통계</Text>
-      <WeekSelector selectedOffset={selectedWeekOffset} onChange={setSelectedWeekOffset} />
-      <Chart data={fileteredData}></Chart>
+      <WeekSelector
+        selectedOffset={selectedWeekOffset}
+        onChange={setSelectedWeekOffset}
+      />
+      <Chart data={data} />
     </View>
-  )
+  );
 }
